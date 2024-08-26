e7abafb3ef11a8e72655bf3ff05f12865265c4e7
RafaelRodrigues17 <rrodriguesmattos27@gmail.com>
2024-08-26 16:54:15 -0300

    atividade 3

---
+++ atv03/script.js
@@ -0,0 +1,26 @@
+function calcular(){
+    var batata1 = parseFloat(document.getElementById("num1").value)
+    var batata2 = parseFloat(document.getElementById("num2").value)
+    var operador = document.getElementById("operador").value
+
+    switch (operador) {
+        case "+": 
+            resultado = batata1 + batata2
+            break;
+        case "-":
+            resultado = batata1 - batata2
+            break;
+        case "*":
+            resultado = batata1 * batata2
+        case "/":
+            if(batata2 !== 0){
+                resultado = batata1 / batata2
+            }else{
+                resultado = "Erro: Divisão por 0 não existe"
+            }
+ 
+        default:
+            resultado = " Operador inválido"          
+    }
+    document.getElementById("resultado").textContent = resultado
+}
